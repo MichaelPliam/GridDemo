13a093f4405d6346b64f681b8ee095906699bec3
MICHAEL B PLIAM <46980126+MichaelPliam@users.noreply.github.com>
2019-08-31 10:05:50 -0700

    Initial commit

---
+++ GridDemo/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿// GridDemo:MainPage.xaml.cs
+// mb pliam
+// aug 31, 2019
+
+// Xamarin.Forms Grid Tutorial
+// https://docs.microsoft.com/en-us/xamarin/get-started/tutorials/grid/?tabs=vswin
+// Create a grid
+// https://docs.microsoft.com/en-us/xamarin/get-started/tutorials/grid/?tutorial-step=2&tabs=vswin
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GridDemo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
